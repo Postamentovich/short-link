3f3577e734968a9f6c9dfcb866fbbe8c3d77d09e
postamentovich <postamentovich@gmail.com>
2019-12-28 22:45:38 +0300

    fix environment

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 app.use(express.json({ extended: true }));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/link", require("./routes/link.routes"));
 app.use("/t", require("./routes/redirect.routes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.send(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = config.get("port") || 5000;
 
